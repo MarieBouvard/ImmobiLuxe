23826533a95df6b9a295420b184899228da26b51
MarieBouvard <mariebouvard@outlook.com>
2021-10-19 10:44:03 +0200

    Update of house model, with associations and scopes

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,15 @@
 class House < ApplicationRecord
     has_one_attached :photo
+    belongs_to :account
+    paginates_per 15
+
+    geocoded_by :address
+    after_validation :geocode
+
+    scope :latest, -> { order created_at: :desc }
+    
+    scope :sold, -> { where(for_sale: true, status: "sold") }
+    scope :for_sale, -> { where(for_sale: true, status: "available") }
+    scope :leased, -> { where(for_sale: false, status: "leased") }
+    scope :for_rent, -> { where(for_sale: false, status: "available") }
 end
