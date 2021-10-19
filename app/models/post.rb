732993d82c11fcbc391bf2c6bff8ddddaa8cd297
MarieBouvard <mariebouvard@outlook.com>
2021-10-19 10:44:54 +0200

    Update posts model with scopes and pagination limit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
     has_one_attached :image
+    paginates_per 10
+
+    scope :active, -> { where(active: true) }
+
+    scope :latest, -> { order created_at: :desc }
 end
