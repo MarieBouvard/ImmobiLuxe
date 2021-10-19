687a064979c7b12c04ffa5092ee1e61859ecd9de
MarieBouvard <mariebouvard@outlook.com>
2021-10-19 10:37:09 +0200

    Update of public controller

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -1,6 +1,16 @@
 class PublicsController < ApplicationController
 
-    def main 
-    end
+    def main
+        if account_signed_in?
+          path = current_account.admin? ? accounts_path : dashboard_path
+          redirect_to path, flash: { success: "Sucessfully signed in. Welcome to Real Estate" } and return
+        end
+        @houses = House.limit(12).latest
+        @posts = Post.limit(4).latest
+      end
+    
+      def index
+        @houses = House.all.page(params[:page]).per(20)
+      end
     
 end
