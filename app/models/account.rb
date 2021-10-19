e20348fa408a33e5861379d2e37d90d52a1746cc
MarieBouvard <mariebouvard@outlook.com>
2021-10-19 10:43:01 +0200

    Update of model Account, with associations definitions and full_name method

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,12 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :houses
+  has_one_attached :image
+  has_one_attached :cover_image
+  paginates_per 15
+       
+  def full_name
+    "#{email}"
+  end
 end
