2037328b0f29f30ee5b13b239150f670fed133df
MarieBouvard <mariebouvard@outlook.com>
2021-10-19 10:39:41 +0200

    Definition of correct user and require login in the house helper

--- app/helpers/houses_helper.rb
+++ app/helpers/houses_helper.rb
@@ -1,2 +1,19 @@
 module HousesHelper
+ 
+    private 
+    #Only login peoples can access to the edit/update/destroy methods. 
+    def require_login
+        unless current_account != nil 
+            redirect_to new_account_session_path
+        end
+    end
+
+    #Only correct user can perform edit/update/destroy on his own houses.
+    def correct_account
+        @house = House.find(params[:id])
+        unless current_account == @house.account
+            redirect_to(@house, notice: "You cannot perform this action")
+        end
+    end
+
 end
