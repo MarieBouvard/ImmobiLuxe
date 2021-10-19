d62bb9017dc6b78988591d4a19f18a4e97a7ece7
MarieBouvard <mariebouvard@outlook.com>
2021-10-19 11:31:48 +0200

    Add of security keys for Google maps api - update of gemfile

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
+ gem 'mini_magick', '~> 4.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -66,6 +66,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'figaro'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-
-
-  resources :posts
-  resources :houses
-  get  "/dashboard" => 'dashboard#index', as: :dashboard
   
   devise_for :accounts
-  root to: "publics#main"
+  resources :houses
+  resources :posts
+
+  get "/blog" => "posts#latest", as: :blog
+  get  "/accounts" => 'admins#accounts', as: :accounts
+  get "/properties" => 'publics#index', as: :properties
+  get  "/dashboard" => 'dashboards#index', as: :dashboard
+  get  "/profile/:id" => 'dashboards#profile', as: :profile
+
+  post "/agent/message" => 'houses#email_agent', as: :email_agent
+  
+  root to: 'publics#main'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
